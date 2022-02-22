208f5f75012dde92ceabc4804404437c1519cc25
Tom Laatz <lato1011@h-ka.de>
2022-02-22 18:17:03 +0100

    yaml dateien

---
+++ extras/postgres/scripts/drop-db-kunde.sql
@@ -0,0 +1,23 @@
+-- Copyright (C) 2022 - present Juergen Zimmermann, Hochschule Karlsruhe
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+-- docker compose exec postgres bash
+-- psql --dbname=postgres --username=postgres --file=/scripts/drop-db-kunde.sql
+
+-- https://www.postgresql.org/docs/14/sql-dropdatabase.html
+DROP DATABASE IF EXISTS bestellung;
+
+-- https://www.postgresql.org/docs/14/sql-droprole.html
+DROP ROLE IF EXISTS bestellung;
